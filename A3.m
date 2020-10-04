78b75d73f243be67c1c16ed4ed1f86d2c26ccf48
sfrogson <sfrogson@utas.edu.au>
2020-10-04 23:16:43 +1100

    Rough start to GUI

--- A3.m
+++ A3.m
@@ -1,6 +1,6 @@
 % Dimensions of the SOM
-s1=3;
-s2=3;
+s1=5;
+s2=5;
 numIter = 10000;
 
 [extremeBanks, healthyBanks, failedBanks] = Bank_data();
@@ -10,24 +10,27 @@ limits = [max(data); min(data)];
 
 normData = normalize(data, 'range')';   % Scale data between 0 to 1
 normExtreme = normData(:, 1:size(extremeBanks, 1));
-normHealthy = normData(:, 1+size(extremeBanks, 1):1+size(extremeBanks, 1)+size(healthyBanks,1));
-normFailed = normData(:, end-size(failedBanks, 1):end);
+normHealthy = normData(:, 1+size(extremeBanks, 1):size(extremeBanks, 1)+size(healthyBanks,1));
+normFailed = normData(:, end-size(failedBanks, 1)+1:end);
 
-net = newsom(norm_data, [s1 s2]);
+net = newsom(normData, [s1 s2]);
 net.iw{1,1} = ones(size(net.iw{1,1}));
 
-% plotsom(net.iw{1,1},net.layers{1}.distances)
+%plotsom(net.iw{1,1},net.layers{1}.distances)
 
 net.trainParam.showWindow = true;
 net.trainParam.epochs = numIter;
-net = train(net, norm_data);
+net = train(net, normData);
 plotsom(net.IW{1,1},net.layers{1}.distances);
 
 % View which nodes each data set triggers
-% plotsomhits(net, normExtreme);
-% plotsomhits(net, normHealthy);
+plotsomhits(net, normExtreme);
+figure
+plotsomhits(net, normHealthy);
+figure
 plotsomhits(net, normFailed);
 
 % Failed banks trigger these nodes
 a = sim(net, normFailed);
 a = mod(find(a), s1*s2)
+
